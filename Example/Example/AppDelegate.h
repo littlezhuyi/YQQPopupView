bf48b29f0debdbcfcb786c98dea4b3ade69c435f
zhuyi <18749618826@163.com>
2020-10-28 17:30:11 +0800

    初始化

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by zhuyi on 2020/10/28.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
