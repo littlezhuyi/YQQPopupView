05905fc8584d27fbcc986a761e80287e7aa3af1b
zhuyi <18749618826@163.com>
2020-10-30 11:11:44 +0800

    修改自定义

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -23,7 +23,9 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     YQQPopupView *popupView;
-//    popupView = [YQQPopupView popupViewWithTitle:@"这是title" detail:@"这是详情"];
+    popupView = [YQQPopupView popupViewWithTitle:@"这是title" detail:@"这是详情"];
+    popupView.useMotionEffects = YES;
+    popupView.closeOnTouchUpOutside = NO;
 //    popupView = [YQQPopupView popupViewWithTitle:@"这是title" detail:@"这是详情" buttonTitles:@[@"cancel"] selectIndex:^(NSInteger index, NSString * _Nullable inputContent) {
 //
 //    }];
@@ -46,12 +48,12 @@
 //    popupView = [YQQPopupView popupViewWithTitle:@"这是title" detail:@"" placeholder:@"请输入" inputType:YQQPopupViewInputTypeTextView buttonTitles:@[@"cancel", @"ok"] buttonType:YQQPopupViewButtonTypeShapeless selectIndex:^(NSInteger index, NSString * _Nullable inputContent) {
 //
 //    }];
-    popupView = [YQQPopupView popupViewWithTitle:@"这是title" detail:@"" inputType:YQQPopupViewInputTypeTextView placeholder:@"请输入" buttonType:YQQPopupViewButtonTypeRoundCorner buttonTitles:@[@"cancel", @"ok"] textViewMaxLength:50 selectIndex:^(NSInteger index, NSString * _Nullable inputContent) {
-        NSLog(@"%@", inputContent);
-    }];
+//    popupView = [YQQPopupView popupViewWithTitle:@"这是title" detail:@"" inputType:YQQPopupViewInputTypeTextView placeholder:@"请输入" buttonType:YQQPopupViewButtonTypeRoundCorner buttonTitles:@[@"cancel", @"ok"] textViewMaxLength:50 selectIndex:^(NSInteger index, NSString * _Nullable inputContent) {
+//        NSLog(@"%@", inputContent);
+//    }];
     [popupView show];
     
-//    YQQCustomPopupView *customeView = [YQQCustomPopupView popupViewWithIimage:[UIImage imageNamed:@"PA_UserVerify_wrongId"] buttonTitles:@[@"cancel", @"ok"] selectIndex:^(NSInteger index, NSString * _Nullable inputContent) {
+//    YQQCustomPopupView *customeView = [YQQCustomPopupView popupViewWithIimage:[UIImage imageNamed:@"404"] buttonTitles:@[@"cancel", @"ok"] selectIndex:^(NSInteger index, NSString * _Nullable inputContent) {
 //
 //    }];
 //    [customeView show];
