abae59132dc6fd4f3190c1911e09c54c238d5d0b
zhuyi <18749618826@163.com>
2020-11-19 10:53:13 +0800

    zhuyi-删除无用文件

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -22,10 +22,10 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    YQQPopupView *popupView;
-    popupView = [YQQPopupView popupViewWithTitle:@"这是title" detail:@"这是详情"];
-    popupView.useMotionEffects = YES;
-    popupView.closeOnTouchUpOutside = NO;
+//    YQQPopupView *popupView;
+//    popupView = [YQQPopupView popupViewWithTitle:@"这是title" detail:@"这是详情"];
+//    popupView.useMotionEffects = YES;
+//    popupView.closeOnTouchUpOutside = NO;
 //    popupView = [YQQPopupView popupViewWithTitle:@"这是title" detail:@"这是详情" buttonTitles:@[@"cancel", @"ok"] selectIndex:^(NSInteger index, NSString * _Nullable inputContent) {
 //
 //    }];
@@ -38,12 +38,12 @@
 //    popupView = [[YQQPopupView alloc] initWithTitle:@"这是title" detail:@"这是详情" placeholder:@"请输入" inputType:YQQPopupViewInputTypeTextView buttonTitles:@[@"cancel", @"ok"] buttonType:YQQPopupViewButtonTypeRoundCorner selectIndex:^(NSInteger index, NSString * _Nullable inputContent) {
 //
 //    }];
-    [popupView show];
+//    [popupView show];
     
-//    YQQCustomPopupView *customeView = [YQQCustomPopupView popupViewWithIimage:[UIImage imageNamed:@"404"] buttonTitles:@[@"cancel", @"ok"] selectIndex:^(NSInteger index, NSString * _Nullable inputContent) {
-//
-//    }];
-//    [customeView show];
+    YQQCustomPopupView *customeView = [YQQCustomPopupView popupViewWithIimage:[UIImage imageNamed:@"404"] buttonTitles:@[@"cancel", @"ok"] selectIndex:^(NSInteger index, NSString * _Nullable inputContent) {
+
+    }];
+    [customeView show];
 }
 
 @end
